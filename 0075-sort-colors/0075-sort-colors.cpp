151a9ca47ee05545a2259f40fe94f4109326e9a4
Kamal Singh Rawat <77820863+KAMAL15402@users.noreply.github.com>
2023-07-19 01:47:55 +0530

    Time: 6 ms (23.89%), Space: 8.3 MB (7.95%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-      int arr[3]={0},n = nums.size();
-        for(int i=0;i<n;i++){
-            arr[nums[i]]++;
-        }
-      /*  for(int i=0;i<3;i++){
-            cout<<arr[i];
-        }*/
-        int c=0;
-        for(int i=0;i<n;i++){
-            if(arr[c] != 0 ){
-            nums[i] = c ;  
-            arr[c]--;    
-            }else{
-                c++;
-                i--;
+        //1st approach use library function :)
+        //2nd  count the no of 0,1,2 and then fill the vector accoriding to it
+        
+        
+        
+        
+        //3 pointer approach
+        int l=0,m=0,h=nums.size()-1;  //l is low , m is medium , h is high 
+        while(m<=h){
+            if(nums[m]==1){
+                m++;
             }
-             
+            else if(nums[m]==0){
+                int temp =  nums[l];
+                nums[l]= 0;
+                nums[m]= temp;
+                l++;
+                m++;
+            }
+            else{
+                int temp =  nums[h];
+                nums[h]= 2;
+                nums[m]= temp;
+                
+                h--;
+                
+            }
         }
     }
 };
