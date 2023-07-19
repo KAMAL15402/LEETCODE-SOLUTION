fd5b4e9fbc9a1f5f15677f6568ebc855c637e190
Kamal Singh Rawat <77820863+KAMAL15402@users.noreply.github.com>
2023-07-20 02:32:22 +0530

    Time: 131 ms (50.61%), Space: 61.2 MB (41.95%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -10,16 +10,27 @@ public:
         //         return nums[i];
         // }
         
-        //negative marking method
         
-        for(int i=0;i<nums.size();i++){
-            if(nums[abs(nums[i])]<0){
-                return abs(nums[i]);
-            }else{
-                nums[abs(nums[i])] *= -1;
+        //2nd method
+        //negative marking method
+        //time complexity o(n)  space o(1)
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[abs(nums[i])]<0){
+//                 return abs(nums[i]);
+//             }else{
+//                 nums[abs(nums[i])] *= -1;
                 
-            }
+//             }
+//         }
+        
+        
+        //3rd solution
+        //positioning method
+        
+        while(nums[0]!=nums[nums[0]]){
+          swap(nums[nums[0]],nums[0]);
         }
+        return nums[0];
         
         
         return -1;
