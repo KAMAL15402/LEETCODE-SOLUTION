85fde7dad3db5526b6a3381122a3bdb0e89b58cd
Kamal Singh Rawat <77820863+KAMAL15402@users.noreply.github.com>
2023-07-20 02:17:03 +0530

    Time: 123 ms (64.21%), Space: 63.6 MB (22.02%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,12 +1,28 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-         //1st sol
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1])
+         //1st sol 
+        //sort the array and then check the adjacent element 
+        //time complexcity - nlogn sort + n checking the adjacent  space 0(n) as space is used while sorting
+        // sort(nums.begin(),nums.end());
+        // for(int i=0;i<nums.size()-1;i++){
+        //     if(nums[i]==nums[i+1])
+        //         return nums[i];
+        // }
+        
+        //negative marking method
+        vector <int> v;
+        v= nums;
+        for(int i=0;i<nums.size();i++){
+            if(v[nums[i]]<0){
                 return nums[i];
+            }else{
+                v[nums[i]] *= -1;
+                
+            }
         }
+        
+        
         return -1;
     }
 };
