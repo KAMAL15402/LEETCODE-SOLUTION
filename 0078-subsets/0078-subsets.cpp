337b3e0d97c104a8e6c551dbb5f3da5d9cf21e1d
Kamal Singh Rawat <77820863+KAMAL15402@users.noreply.github.com>
2023-08-02 01:54:41 +0530

    Time: 0 ms (100%), Space: 16.6 MB (18.01%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,3 +1,7 @@
+/** 
+                   ITERATIVE APPROACH   GOOD TIME BUT TAKES MORE STORAGE 
+                   TIME COMPLEXITY - (N*2^N)    SPACE COMPLEXITY -   (  N* 2^N )
+
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
@@ -13,5 +17,32 @@ public:
             }
         }  
         return v;
+    }
+};
+**/
+
+
+
+class Solution {
+public:
+    void solve(vector <int> nums,vector <int> o,int index,vector<vector<int>>& ans){
+       
+        if(index >= nums.size()){
+            ans.push_back(o);
+            return ;
+        } 
+        
+        solve(nums, o,index+1,ans);
+        int element = nums[index];
+        o.push_back(element);
+        solve(nums, o,index+1,ans);
+        
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> o ;
+        int index=0;
+        solve(nums,o,index,ans);
+        return ans;
     }
 };
