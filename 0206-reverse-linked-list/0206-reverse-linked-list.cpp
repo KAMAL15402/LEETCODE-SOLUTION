82b22820c4ed6a0058a1110e89792a2ded15b7ed
Kamal Singh Rawat <77820863+KAMAL15402@users.noreply.github.com>
2023-09-07 21:48:48 +0530

    Time: 8 ms (30.93%), Space: 8.5 MB (17.03%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -8,18 +8,44 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+/*   ITERATIVE METHOD  TC - O(N) SPACE = O(3N)
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         ListNode* pre = NULL;
         ListNode* curr = head;
-        while(curr!=NULL){
-            ListNode* temp = curr->next; 
+        ListNode* post = NULL;
+        if(head == NULL){
+            return NULL;
+        }
+        while(curr!=NULL &&  curr->next!=NULL ){
+            post = curr->next; 
             curr->next = pre;
             pre = curr;
-            curr = temp; 
+            curr = post; 
          }
-         //curr->next = pre;
-         return pre;
+         curr->next = pre;
+         return curr;
+    }
+};
+*/
+
+class Solution {
+public:
+    ListNode* reverse(ListNode* head){
+        if(head->next==NULL){
+            return head;
+        }
+        ListNode* reverseHead = reverse(head->next);
+        head->next->next = head;//draw and think
+        head->next = NULL;
+        return reverseHead;
+    } 
+
+    ListNode* reverseList(ListNode* head) {
+      if(head==NULL){
+          return NULL;
+      }
+      return reverse(head);  
     }
 };
