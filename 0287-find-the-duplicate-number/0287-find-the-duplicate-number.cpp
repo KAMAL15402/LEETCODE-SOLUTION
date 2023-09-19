a32a158946b42d22023fce8b036835e2ccc4f28a
Kamal Singh Rawat <77820863+KAMAL15402@users.noreply.github.com>
2023-09-19 09:39:09 +0530

    Time: 74 ms (88.03%), Space: 61.6 MB (28.42%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,38 +1,23 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-         //1st sol 
-        //sort the array and then check the adjacent element 
-        //time complexcity - nlogn sort + n checking the adjacent  space 0(n) as space is used while sorting
-        // sort(nums.begin(),nums.end());
-        // for(int i=0;i<nums.size()-1;i++){
-        //     if(nums[i]==nums[i+1])
-        //         return nums[i];
-        // }
-        
-        
-        //2nd method
-        //negative marking method
-        //time complexity o(n)  space o(1)
-//         for(int i=0;i<nums.size();i++){
-//             if(nums[abs(nums[i])]<0){
-//                 return abs(nums[i]);
-//             }else{
-//                 nums[abs(nums[i])] *= -1;
-                
-//             }
-//         }
-        
-        
-        //3rd solution
-        //positioning method
-        
-        while(nums[0]!=nums[nums[0]]){
-          swap(nums[nums[0]],nums[0]);
-        }
-        return nums[0];
-        
-        
-        return -1;
+    int slow = nums[0];
+    int fast = nums[0];
+
+    // Phase 1: Detect if there's a cycle in the array
+    do {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while (slow != fast);
+
+    // Phase 2: Find the entrance to the cycle
+    slow = nums[0];
+    while (slow != fast) {
+        slow = nums[slow];
+        fast = nums[fast];
     }
+
+    return slow;
+}
+
 };
