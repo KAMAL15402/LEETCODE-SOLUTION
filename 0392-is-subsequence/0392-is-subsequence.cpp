88e46b9d05e982c3557f388752d0da77b65054c8
Kamal Singh Rawat <77820863+KAMAL15402@users.noreply.github.com>
2023-09-22 11:40:02 +0530

    Time: 3 ms (49.1%), Space: 6.7 MB (17.6%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0;
+        for(auto c: t){
+            if(i==s.length()){
+                return true;
+            }
+       if(c==s[i]){
+           i++;
+       }
+        }
+        if(i==s.length()){
+                return true;
+            }
+        return false;
+    }
+};
