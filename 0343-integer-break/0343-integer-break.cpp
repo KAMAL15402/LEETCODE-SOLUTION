710378cb9dae13d9149b36b8e23fb830d3b4dc89
Kamal Singh Rawat <77820863+KAMAL15402@users.noreply.github.com>
2023-10-06 10:44:00 +0530

    Time: 3 ms (30.5%), Space: 6.6 MB (22.36%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -6,7 +6,7 @@ public:
       
         for (int i = 2; i <= n; ++i) {
             for (int j = 1; j < i; ++j) {
-                dp[i] = max(dp[i], max(j * (i - j), j * dp[i - j]));
+                dp[i] = max({dp[i], j * (i - j), j * dp[i - j]});
             }
         }
         return dp[n];
