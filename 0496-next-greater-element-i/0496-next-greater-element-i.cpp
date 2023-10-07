a86a298ce334a9e92bfc75f525d61a0c1b8bf6b5
Kamal Singh Rawat <77820863+KAMAL15402@users.noreply.github.com>
2023-10-07 23:30:35 +0530

    Time: 7 ms (46.7%), Space: 9.6 MB (7.84%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,45 +1,21 @@
 class Solution {
 public:
-      /*        pure brute force
-      vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int> m;
+        stack <int> s;
+        for(int i=nums2.size()-1;i>=0;i--){
+            while(!s.empty() && nums2[i]>s.top()){
+                s.pop();
+            }
+            int result = (s.empty())?-1:s.top();
+            m.insert({nums2[i],result});
+            s.push(nums2[i]);
+        }
+
         vector<int> ans;
-        for(int i=0;i<nums1.size();i++){
-            int index = -1,j=0;
-            for( j=0;j<nums2.size();j++){
-                if(nums1[i]==nums2[j]){
-                    index = -2;
-                }
-                if(nums1[i]<nums2[j] && index == -2){
-                    ans.push_back(nums2[j]);
-                    break;
-                }
-            }
-            if(j==nums2.size()){
-                ans.push_back(-1);
-            }
+        for(auto x: nums1){
+            ans.push_back(m[x]);
         }
         return ans;
     }
-      */
-
-
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-         unordered_map <int,int> umap;
-         stack <int> st;
-         int n = nums2.size();
-         for(int i=n-1;i>=0;i--){
-             int ele = nums2[i];
-             while(!st.empty()&& st.top()<= ele){
-               st.pop();
-             }
-             int result = (st.empty())?-1:st.top();
-             umap.insert({ele,result});
-             st.push(ele); 
-         }
-         vector <int> ans ;
-         for(auto x: nums1){
-             ans.push_back(umap[x]);
-         }
-         return ans;
-    } 
 };
