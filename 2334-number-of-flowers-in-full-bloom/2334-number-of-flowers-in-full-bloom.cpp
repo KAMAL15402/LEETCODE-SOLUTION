9b5f6b796a835bca8b47ad47cea81aa3b10cdf95
Kamal Singh Rawat <77820863+KAMAL15402@users.noreply.github.com>
2023-10-11 14:08:09 +0530

    Time: 300 ms (25.59%), Space: 94.3 MB (26.68%) - LeetHub

--- 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.cpp
+++ 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& persons) {
-       vector<pair<int, int> > pairs;
-        for (int i=0;i<persons.size(); i++) 
-        pairs.push_back({persons[i], i}); 
-        sort (pairs.begin(), pairs.end()); 
-        sort (flowers.begin(), flowers.end()); 
-        int l=0; 
-        vector<int>ans (persons.size()); 
-        multiset<int> S; 
-        for (int i=0;i<pairs.size();i++) {
-        while (l<flowers.size() && pairs[i].first>=flowers [l][0]) 
-        S.insert (flowers [l][1]), l++;
-     while (S.size() && (*(S.begin()) <pairs [i].first)) 
-     S.erase (S.begin()); 
-     ans [pairs[i].second] =S.size();
+    vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& people) {
+        vector<pair<int,int> >pairs;
+        for(int i= 0;i<people.size();i++){
+            pairs.push_back({people[i],i});
+        }
+        sort(pairs.begin(),pairs.end());
+        sort(flowers.begin(),flowers.end());
+        int l = 0;
+        vector<int> ans(people.size());
+        multiset<int> S;
+        for(int i=0;i<pairs.size();i++){
+            while(l<flowers.size()&& pairs[i].first >= flowers[l][0]){
+                S.insert(flowers[l][1]);
+                l++;
+                
+            }
+            while(S.size() && (*(S.begin())<pairs[i].first)){
+               S.erase(S.begin());
+
+            }
+            
+            ans[pairs[i].second] = S.size();
         }
         return ans;
     }
